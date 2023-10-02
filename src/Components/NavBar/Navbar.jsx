6c88970c2884dac301c41d7c3af920632ed06c8f
Nico Pedernera <nicopedernera@hotmail.com.ar>
2023-10-02 20:44:07 -0300

    Pre entrega 1

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Cart</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">About Us</a>
